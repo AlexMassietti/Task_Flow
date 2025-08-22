dc03685c34848be86512d36edba5a4c30f33dd78
Julián <julianfermani45@gmail.com>
2025-08-22 11:32:49 -0300

    docs: añado swagger + docs de permisos

--- Backend/microservice-user/src/modules/permissions/dto/create-permission.dto.ts
+++ Backend/microservice-user/src/modules/permissions/dto/create-permission.dto.ts
@@ -1,9 +1,16 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreatePermissionDto {
   @IsString()
+  @ApiProperty({
+    description: 'Nombre del permiso',
+  })
   name: string;
 
   @IsString()
+  @ApiProperty({
+    description: 'Descripción del permiso',
+  })
   description: string;
 }
--- Backend/microservice-user/src/modules/permissions/permissions.controller.ts
+++ Backend/microservice-user/src/modules/permissions/permissions.controller.ts
@@ -10,27 +10,51 @@ import {
 import { PermissionsService } from './permissions.service';
 import { CreatePermissionDto } from './dto/create-permission.dto';
 import { UpdatePermissionDto } from './dto/update-permission.dto';
+import { ApiTags, ApiOperation, ApiResponse, ApiParam } from '@nestjs/swagger';
+import { Permission } from './entities/permission.entity';
 
 @Controller('permissions')
 export class PermissionsController {
   constructor(private readonly permissionsService: PermissionsService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Crear un nuevo permiso' })
+  @ApiResponse({
+    status: 201,
+    description: 'Permiso creado',
+    type: Permission,
+  })
   create(@Body() createPermissionDto: CreatePermissionDto) {
     return this.permissionsService.create(createPermissionDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Obtener todos los permisos' })
+  @ApiResponse({
+    status: 200,
+    description: 'Lista de permisos',
+    type: [Permission],
+  })
   findAll() {
     return this.permissionsService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Obtener un permiso por id' })
+  @ApiParam({ name: 'id', type: Number })
+  @ApiResponse({
+    status: 200,
+    description: 'Permiso encontrado',
+    type: Permission,
+  })
   findOne(@Param('id') id: string) {
     return this.permissionsService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Actualizar un permiso' })
+  @ApiParam({ name: 'id', type: Number })
+  @ApiResponse({ status: 200, description: 'Permiso actualizado' })
   update(
     @Param('id') id: string,
     @Body() updatePermissionDto: UpdatePermissionDto,
@@ -39,6 +63,9 @@ export class PermissionsController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Eliminar un permiso' })
+  @ApiParam({ name: 'id', type: Number })
+  @ApiResponse({ status: 200, description: 'Permiso eliminado' })
   remove(@Param('id') id: string) {
     return this.permissionsService.remove(+id);
   }
