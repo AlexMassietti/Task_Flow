da6a1cf4b517129439a8bfac24d59db1a22e3a28
Julián <julianfermani45@gmail.com>
2025-11-11 08:37:20 -0300

    feat: empiezo /dashboard/{userID}/get-owned-dashboards

--- ApiGateway/api-gateway/src/app.module.ts
+++ ApiGateway/api-gateway/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './modules/app.controller';
 import { AppService } from './modules/app.service';
 import { ClientsModule } from '@nestjs/microservices';
-import { DASHBOARD_SERVICE, USERS_SERVICE } from './config/miroservice.config';
+import { DASHBOARD_SERVICE, USERS_SERVICE } from './config/microservice.config';
 
 @Module({
   imports: [ClientsModule.register([USERS_SERVICE, DASHBOARD_SERVICE])],
