739dfb5db99e5eb65ba360c7e6318eb0d3a1a448
Julian <julianfermani45@gmail.com>
2025-12-22 12:40:00 +0000

    refactor(db): creo infra module + updateo imports

--- apps/microservice-tasks/src/app.module.ts
+++ apps/microservice-tasks/src/app.module.ts
@@ -3,19 +3,21 @@ import { TaskModule } from './task/task.module';
 import { StatusModule } from './status/status.module';
 import { PriorityModule } from './priority/priority.module';
 import { DashboardModule } from './dashboard/dashboard.module';
-import { SeedModule } from './seed/seed.module';
 import { RolDashboardModule } from './rol-dashboard/rol-dashboard.module';
 import { ParticipantTypeModule } from './participant-type/participant-type.module';
 import { DatabaseModule } from './database/database.module';
+import { CoreModule } from './core/core.module';
+import { InfraModule } from './infra/infra.module';
 
 @Module({
   imports: [
     DatabaseModule,
+    CoreModule,
+    InfraModule,
     TaskModule,
     StatusModule,
     PriorityModule,
     DashboardModule,
-    SeedModule,
     RolDashboardModule,
     ParticipantTypeModule,
   ],
