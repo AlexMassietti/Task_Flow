02c0d462a7e32043e35d0770918c1602aa2bd0d4
Julian <julianfermani45@gmail.com>
2025-12-22 11:49:22 +0000

    refactor(db): saco config de la db afuera + parche

--- apps/microservice-tasks/src/app.module.ts
+++ apps/microservice-tasks/src/app.module.ts
@@ -3,23 +3,14 @@ import { TaskModule } from './task/task.module';
 import { StatusModule } from './status/status.module';
 import { PriorityModule } from './priority/priority.module';
 import { DashboardModule } from './dashboard/dashboard.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { SeedModule } from './seed/seed.module';
 import { RolDashboardModule } from './rol-dashboard/rol-dashboard.module';
 import { ParticipantTypeModule } from './participant-type/participant-type.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5431,
-      username: 'postgres',
-      password: 'taskDatabase',
-      database: 'task',
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
+    DatabaseModule,
     TaskModule,
     StatusModule,
     PriorityModule,
@@ -31,4 +22,4 @@ import { ParticipantTypeModule } from './participant-type/participant-type.modul
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
--- apps/microservice-tasks/src/seed/seed.module.ts
+++ apps/microservice-tasks/src/seed/seed.module.ts
@@ -1,25 +1,16 @@
 import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { TaskModule } from '@microservice-tasks/task/task.module';
 import { StatusModule } from '@microservice-tasks/status/status.module';
 import { PriorityModule } from '@microservice-tasks/priority/priority.module';
 import { DashboardModule } from '@microservice-tasks/dashboard/dashboard.module';
 import { ParticipantTypeModule } from '@microservice-tasks/participant-type/participant-type.module';
 import { RolDashboardModule } from '@microservice-tasks/rol-dashboard/rol-dashboard.module';
+import { DatabaseModule } from '@microservice-tasks/database/database.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5431,
-      username: 'postgres',
-      password: 'taskDatabase',
-      database: 'task',
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
+    DatabaseModule,
     TaskModule,
     StatusModule,
     PriorityModule,
@@ -30,4 +21,4 @@ import { RolDashboardModule } from '@microservice-tasks/rol-dashboard/rol-dashbo
   controllers: [],
   providers: [SeedService],
 })
-export class SeedModule {}
+export class SeedModule { }
